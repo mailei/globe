c016bd27ddca3f607f677a603a9b7c491fc001d4
mailei <memorycross@gmail.com>
2017-12-11 23:53:40 +0900

    カメラ移動

--- public/javascripts/earth.js
+++ public/javascripts/earth.js
@@ -9,19 +9,18 @@ var meshEarth;
 var isMouseDown = false;
 
 init();
-render();
+animate();
 
 function init() {
 
-  container = document.createElement('div');
-  document.body.appendChild(container);
+  // container = document.createElement('div');
+  // document.body.appendChild(container);
 
   // three.jsの設定 シーン作成
   scene = new THREE.Scene();
 
-  //カメラ作成
-  camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 1000;
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 2000);
+  camera.position.z = 2000;
 
   //レンダラ設定
   renderer = new THREE.WebGLRenderer();
@@ -39,17 +38,42 @@ function init() {
 
 
   // 地球
-  var geometryEarth = new THREE.SphereGeometry(280, 64, 64);
-  var materialEarth = new THREE.MeshPhongMaterial({ color: 0xffffff, map: THREE.ImageUtils.loadTexture('../images/earth.jpg') });
+  var geometryEarth = new THREE.SphereGeometry(140, 32, 32);
+  var materialEarth = new THREE.MeshBasicMaterial({
+    map: THREE.ImageUtils.loadTexture('../images/earth.jpg')
+  });
   // 形と素材を引数として渡す
   meshEarth = new THREE.Mesh(geometryEarth, materialEarth);
-
-  earthRot += 0.001;
-  meshEarth.position.x = Math.sin(earthRot) * 3800;
-  meshEarth.position.z = Math.cos(earthRot) * 3800;
-  meshEarth.rotation.x = -Math.PI * (1 / 8);
-  meshEarth.rotation.y += 0.008;
+  meshEarth.position.x = 500;
+  meshEarth.position.y = 500;
+  meshEarth.position.z = 500;
   scene.add(meshEarth);
+
+
+  // 月
+  var geometryMoon = new THREE.SphereGeometry(30, 30, 34);
+  var materialMoon = new THREE.MeshBasicMaterial({
+    map: THREE.ImageUtils.loadTexture('../images/moon.jpg')
+  });
+  meshMoon = new THREE.Mesh(geometryMoon, materialMoon);
+  // 生成したオブジェクトの位置
+  meshMoon.position.x = 400;
+  meshMoon.position.y = 400;
+  meshMoon.position.z = 400;
+  scene.add(meshMoon);
+
+  // 太陽
+  var geometrySun = new THREE.SphereGeometry(100, 64, 64);
+  var materialSun = new THREE.MeshBasicMaterial({
+    map: THREE.ImageUtils.loadTexture('../images/sun.jpg')
+  });
+  meshSun = new THREE.Mesh(geometrySun, materialSun);
+  // 生成したオブジェクトの位置
+  // meshSun.position.x = 500;
+  // meshSun.position.y = 500;
+  // meshSun.position.z = 500;
+  scene.add(meshSun);
+
 
   //window設定
   var text = {
@@ -74,56 +98,48 @@ function init() {
     if (!value) meshEarth.visible = false;
   });
   saizu.onChange(function (value) {
+    // meshEarth.scale.set(value, value, value);
     meshEarth.scale.set(value, value, value);
   });
 
-  // マウス、タッチ処理を呼び出すイベントリスナーをセット
-  document.addEventListener("mousedown", onMouseDown);
-  document.addEventListener("touchstart", onMouseDown);
-  document.addEventListener("mouseup", onMouseUp);
-  document.addEventListener("touchend", onMouseUp);
-  document.addEventListener("mousemove", onMouseMove);
-  document.addEventListener("touchmove", onMouseMove);
+  //　カメラを動かすためのオブジェクトコントロール
+  controls = new THREE.OrbitControls(camera);
 
 }
 
-// 描画処理
-function render() {
-  requestAnimationFrame(render);
+// // 描画処理
+// function render() {
+//   requestAnimationFrame(render);
+//   // カメラをアップデート
+//   controls.update();
+//   renderer.render(scene, camera);
+// }
+
+// animations
+function animate() {
+  // フレーム内処理
+  requestAnimationFrame(animate);
+
+  // // 地球の位置
+  // earthRot += 0.001;
+  // meshEarth.position.x = Math.sin(earthRot) * 3800;
+  // meshEarth.position.z = Math.cos(earthRot) * 3800;
+  // meshEarth.rotation.x = -Math.PI * (1 / 8);
+  // meshEarth.rotation.y += 0.008;
+
+  // 月の位置
+  // moonRot += 0.01;
+  // meshMoon.position.x = Math.sin(moonRot) * 500 + meshEarth.position.x;
+  // meshMoon.position.z = Math.cos(moonRot) * 500 + meshEarth.position.z;
+  // meshMoon.rotation.y += 0.001;
+
+  // カメラの位置
+  // camera.position.x = -1000;
+  // camera.position.z = -5000;
+  // camera.position.y = 0;
+  // camera.lookAt({ x: meshEarth.position.x, y: meshEarth.position.y, z: meshEarth.position.z });
+  controls.update();
   renderer.render(scene, camera);
+
 }
 
-// マウスを押したとき
-function onMouseDown(event) {
-  isMouseDown = true;
-}
-
-// マウスを動かした時
-function onMouseMove(event) {
-  if (isMouseDown) {
-    // 3DモデルをX軸とY軸方向に回転させます
-    if (meshEarth) {
-      meshEarth.rotation.y = getMouseX(event) / 50;
-      meshEarth.rotation.x = getMouseY(event) / 50;
-    }
-  }
-}
-
-// マウスを離したとき
-function onMouseUp(event) {
-  isMouseDown = false;
-}
-
-function getMouseX(event) {
-  if (event.type.indexOf("touch") == -1)
-    return event.clientX;
-  else
-    return event.touches[0].clientX;
-}
-
-function getMouseY(event) {
-  if (event.type.indexOf("touch") == -1)
-    return event.clientY;
-  else
-    return event.touches[0].clientY;
-}
